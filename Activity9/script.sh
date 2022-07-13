5023121766a05af639c492f8aed527f3189ff91c
Travis Lee <travisslee223@gmail.com>
2022-07-13 21:04:04 +0000

    finish 9

--- Activity9/script.sh
+++ Activity9/script.sh
@@ -2,12 +2,18 @@
 
 function checkInput () {
 	isGood="false"
-
+	dir=/home/ec2-user/scripts/Linux_scripts/Activity9
 	while [ $isGood ==  "false" ]; do
 
                 read -p "Create a file name " a
 		isGood="true"
-		
-		
+		file="$dir/$a.log"
 	done
+	if test -f "$file" ; then
+    		echo "$a exists."
+	else
+		touch $file
+	fi
 }
+
+checkInput
